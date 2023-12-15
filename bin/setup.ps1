db5f94bed2cd27ec0ca270e55f60659a4fa67bae
UnPySide <satellitear@gmail.com>
2023-12-15 23:24:37 +0900

    chore: establish local setup for Python environment configuration
    - Implemented a local setup environment for configuring Python development settings.
    - This setup includes necessary scripts and configuration files to streamline the Python environment initialization process.

---
+++ bin/setup.ps1
@@ -0,0 +1,29 @@
+﻿$binDir = (Get-Item $MyInvocation.MyCommand.Path).DirectoryName
+$rootDir = (Get-Item $binDir).Parent.FullName
+Set-Location $rootDir
+
+$pythonVer = New-Object System.Version(3, 11, 6)
+Write-Output ("Python=={0}" -f $pythonVer)
+
+$nuGetExe = Join-Path $binDir "nuget.exe"
+$pythonDir = Join-Path $binDir "python"
+$pythonPath = Join-Path $pythonDir "tools\python.exe"
+
+
+function InstallPython {
+    $psi = New-Object System.Diagnostics.ProcessStartInfo
+    $psi.FileName = $nuGetExe
+    $psi.Arguments = "install python -Version {0} -ExcludeVersion -OutputDirectory ""{1}""" -f $pythonVer.ToString(3), $binDir
+    $psi.UseShellExecute = $false
+    $psi.CreateNoWindow = $false
+    $p = [System.Diagnostics.Process]::Start($psi)
+    $p.WaitForExit()
+}
+
+
+if (Test-Path $pythonDir) {
+    Remove-Item $pythonDir -Recurse -Force
+}
+
+InstallPython
+Write-Output "Installed Python"
