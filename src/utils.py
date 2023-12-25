d1d2407efcaa1ea06e273980795d465a2b47b7dc
UnPySide <satellitear@gmail.com>
2023-12-26 00:20:32 +0900

    fix: correct non-critical code issues for improved quality
    - Addressed and fixed code issues that were not affecting execution but were potential bugs.
    - This fix enhances the overall quality and maintainability of the codebase.

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,27 @@ from typing import Any
 
 
 def stop_watch(function: Any) -> Any:
+    """
+    A decorator that measures and prints the execution time of a function.
+
+    This decorator records the start and end times when the specified function is called.
+    Upon completion of the function execution, the elapsed time (in seconds) is printed to the standard output.
+    This decorator is useful for performance analysis or debugging to understand the execution time of a function.
+
+    Parameters:
+    function (Any): The function whose execution time you want to measure.
+
+    Returns:
+    Any: The return value of the wrapped function.
+
+    Usage:
+    @stop_watch
+    def sample_function():
+        # Some process
+
+    sample_function()  # Execution time will be printed
+    """
+
     @wraps(function)
     def wrapper(*args: Any, **kargs: Any) -> Any:
         start = time.time()
